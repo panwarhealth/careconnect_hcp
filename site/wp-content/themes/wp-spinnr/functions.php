45a3caa35cf452f3cd925186ff58713358959be2
Doobly <rob@smashingpixels.com.au>
2026-07-23 13:31:53 +1000

    Spinnr fork: restore REST field enrichment used by [wp-post-list]
    custom-rest-api.php also carried the rest_prepare filters that add
    featured_image_url/author_name/ACF to REST responses; the child blog card
    grid renders via internal rest_do_request and lost its thumbnails. New
    inc/rest-fields.php keeps only the enrichment (no spinnr routes, no
    raw_html, no draft exposure).

--- site/wp-content/themes/wp-spinnr/functions.php
+++ site/wp-content/themes/wp-spinnr/functions.php
@@ -16,6 +16,7 @@ require get_template_directory() . '/inc/frontend.php';
 require get_template_directory() . '/inc/setup.php';
 require get_template_directory() . '/inc/layout.php';
 require get_template_directory() . '/inc/enqueue.php';
+require get_template_directory() . '/inc/rest-fields.php';
 require get_template_directory() . '/inc/custom-shortcodes.php';
 require get_template_directory() . '/inc/editor-lock.php';
 require get_template_directory() . '/inc/template-tags.php';
