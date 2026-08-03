a635ea4015566557a90f56286d8de7e43e823fd8
Doobly <rob@smashingpixels.com.au>
2026-08-03 14:20:51 +1000

    search results: drop helpers stripped in spinnr fork (fatal on any non-empty search)

--- site/wp-content/themes/wp-spinnr/template-parts/content-search.php
+++ site/wp-content/themes/wp-spinnr/template-parts/content-search.php
@@ -15,7 +15,7 @@
 
 		<?php if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
-			<?php wp_spinnr_posted_on(); ?>
+			<?php echo esc_html( get_the_date() ); ?>
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
@@ -23,8 +23,4 @@
 	<div class="entry-summary">
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-summary -->
-
-	<footer class="entry-footer">
-		<?php wp_spinnr_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
